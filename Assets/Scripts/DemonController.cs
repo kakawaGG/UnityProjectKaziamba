5718c137a30dbe7b00d81df152d1cbdde186f110
David <bykov-david@mail.ru>
2020-01-14 19:10:31 +0300

    -Добавлен Джойстик

---
+++ Assets/Scripts/DemonController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemonController : MonoBehaviour
+{
+    public float speed;
+    public Vector3 targetMove;
+
+    Rigidbody2D body;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        body.velocity = new Vector2(targetMove.x * speed, targetMove.y * speed);
+    }
+}
---
+++ Assets/Scripts/JoystickController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickController : MonoBehaviour
+{
+    public GameObject JoystickTouch;
+    Vector3 targetVector;
+    public DemonController demon;
+
+    void Start()
+    {
+        JoystickTouch.transform.position = transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 touchPos = Input.mousePosition;
+            targetVector = touchPos - transform.position;
+
+            if (targetVector.magnitude < 100)
+            {
+                JoystickTouch.transform.position = touchPos;
+                demon.targetMove = targetVector;
+            }
+        }
+        else
+        {
+            JoystickTouch.transform.position = transform.position;
+            demon.targetMove = new Vector3(0, 0, 0);
+        }
+    }
+}
