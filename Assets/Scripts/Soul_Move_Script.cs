2adc5bef5aa136da0120c62ba69aa08fa0dfccb6
AlinGod <terass@mail.ru>
2020-01-17 15:28:01 +0300

    ---

--- Assets/Scripts/Soul_Move_Script.cs
+++ Assets/Scripts/Soul_Move_Script.cs
@@ -5,21 +5,18 @@ using UnityEngine;
 
 public class Soul_Move_Script : MonoBehaviour
 {
-    public float speed = 0.4f;
+    public float speed = 0.7f;
     float cur_speed = 0;
     public List<GameObject> agn;
     Vector2 H = new Vector2(0, 0);
     GameObject Demon;
-
-    int rs = 5;
-    int ra = 1;
-
+    bool demon_near = false;
 
     float pa = 0.2f;			// relative strength of repulsion from other agents - 2
     float c = 0.105f;		// relative strength of attraction to the n nearest neighbours - 1.05
     float ps = 0.15f;		// relative strength of repulsion from the shepherd - 1
     float h = 0.05f;			// relative strength of proceeding in the previous direction - 0.5
-    float e = 0f;	
+    float e = 0.02f;	
 
     // Start is called before the first frame update
     void Start()
@@ -32,36 +29,26 @@ public class Soul_Move_Script : MonoBehaviour
     {
         Vector2 LCM = new Vector2(0, 0);
 
-        // Agent and Shepherd position
-        Vector2 A = GetComponent<Rigidbody2D>().position;
-        Vector2 S = Demon.GetComponent<Rigidbody2D>().position;
-
-        //LCM
         int counter = 0;
         int l = agn.Count;
         for (int i = 0; i < l; i++)
         {
-            if (agn[i].GetComponent<Rigidbody2D>().position != A)
-            {
-                if ((agn[i].GetComponent<Rigidbody2D>().position - A).magnitude < rs)
-                {
-                    LCM += agn[i].GetComponent<Rigidbody2D>().position;
-                    counter++;
-                }
-            }
+            LCM += agn[i].GetComponent<Rigidbody2D>().position;
+            counter++;
         }
         if (counter != 0)
             LCM /= counter;
         else
             LCM = GetComponent<Rigidbody2D>().position;
         
-        
+        // Agent and Shepherd position
+        Vector2 A = GetComponent<Rigidbody2D>().position;
         Vector2 Rs;
         Vector2 C;
-        //if (Demon != null)
-        if ((A - S).magnitude <= rs)
+        if (demon_near)
         {
             //Distance between Agent and Shepherd
+            Vector2 S = Demon.GetComponent<Rigidbody2D>().position;
             Rs = A - S;
             C = LCM - A;
             cur_speed = speed;
@@ -71,26 +58,18 @@ public class Soul_Move_Script : MonoBehaviour
             Rs = Vector2.zero;
             C = Vector2.zero;
             if (cur_speed > 0)
-                cur_speed -= 0.1f;
+                cur_speed = cur_speed - 0.1f;
         }
 
         // Local repulsion of Agent from nearest neighbours
         Vector2 Ra = Vector2.zero;
         for (int i = 0; i < l; i++)
         {
-            Vector2 AgnPos = agn[i].GetComponent<Rigidbody2D>().position;
-            if (AgnPos != A)
-            {
-                if ((A - AgnPos).magnitude < ra)
-                {
-                    Ra += (A - AgnPos) / (A - AgnPos).magnitude;
-                }
-            }
-            //Ra += (GetComponent<Rigidbody2D>().position - agn[i].GetComponent<Rigidbody2D>().position).normalized;
+            Ra += (GetComponent<Rigidbody2D>().position - agn[i].GetComponent<Rigidbody2D>().position).normalized;
         }
 
         //Error term
-        Vector2 E = new Vector2(100f * (Random.value - 0.5f), 100f * (Random.value - 0.5f));
+        Vector2 E = new Vector2(10f * (Random.value - 0.5f), 10f * (Random.value - 0.5f));
         
         H = h * H.normalized + c * C.normalized + pa * Ra.normalized + ps * Rs.normalized + e * E.normalized;
         transform.position = A + cur_speed * H.normalized; 
@@ -106,6 +85,7 @@ public class Soul_Move_Script : MonoBehaviour
         else if (collision.gameObject.tag == "Demon")
         {
             Demon = collision.gameObject;
+            demon_near = true;
             
         }
 
@@ -117,6 +97,7 @@ public class Soul_Move_Script : MonoBehaviour
         if (collider.gameObject.tag == "Demon")
         {
             Demon = null;
+            demon_near = false;
         }
         else if (collider.gameObject.tag == "Soul")
         {
